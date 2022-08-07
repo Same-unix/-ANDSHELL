d98ff7e6d46138cc599c76d237562821578adda8
Same-unix <110787671+Same-unix@users.noreply.github.com>
2022-08-07 15:30:40 -0500

    Create Shell.sh

---
+++ Shell.sh
@@ -0,0 +1,16 @@
+!#/bin/bash
+
+# greet
+GREET="hello $USERNAME"
+echo $GREET
+USERNAME="user"
+
+# shell
+while true; do
+read -p "user@github:~$ " yn
+case $yn in
+[Yy]* ) break;;
+[Nn]* ) exit;;
+* ) echo "srry for the yn"
+esac
+done
